eb79f9ec337401581be232fcbbc535a0c35c174b
jrodowick <jrodowick@mail.csuchico.edu>
2019-03-04 13:30:49 -0800

    Refactored syntax

--- Javascript/Q2.js
+++ Javascript/Q2.js
@@ -4,19 +4,19 @@
 // example: answer([1,2,3], 4)should return [1,3]
 
 const targetNum = (array,target) => {
-  let answer = [];
+
   for(let i = 0; i < array.length; i++) {
     let num = array[i];
     for(let e = i+1; e < array.length; e++) {
       if(num + array[e] === target) {
-        temp = [num, array[e]];
-        return temp;
+        let answer = [num, array[e]];
+        return answer;
       }
     }
   }
 }
 
 let array = [1,2,3,4,5,6,7,8,9];
-let target = 15;
+let target = 11;
 
 targetNum(array,target);
